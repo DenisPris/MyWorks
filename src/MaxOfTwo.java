6b7b5959767048ca2d42157d1f18f987a042a1b3
DenisPris <prisyazhnyukdv@gmail.com>
2021-04-28 18:20:46 +0300

    POGROM-3 changed comparison to library class

--- src/MaxOfTwo.java
+++ src/MaxOfTwo.java
@@ -10,10 +10,6 @@ public class MaxOfTwo {
     public static void main(String[] args) {
         int mathRand = (int) (Math.random()*100);
         int mathRand2 = (int) (Math.random()*100);
-        System.out.println("Это рандомное число 1 :" + mathRand);
-        System.out.println("Это рандомное число 2 :" + mathRand2);
-        if (mathRand2 < mathRand) {
-            System.out.println(mathRand + " больше");}
-        else System.out.println(mathRand2 + " больше");
+        System.out.println(Math.max(mathRand, mathRand2));
     }
 }
