120a09126533325befde7eb86811fc1ef0d21e3b
gamingpc <prisyazhnyukdv@gmail.com>
2021-05-14 10:42:17 +0300

    fixed a bug when entering the quantity of numbers more than twenty

--- src/MaxOfTwentyComparisNumber.java
+++ src/MaxOfTwentyComparisNumber.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 
 public class MaxOfTwentyComparisNumber {
     public static void main(String[] args) {
+        System.out.println("Enter the quantity of numbers from one to twenty ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number <= 20) {
+            System.out.println("valid input! " + number);
 
-        Scanner s = new Scanner(System.in);
+            int[] mas = new int[number];
+            int max = mas[0];
 
-        System.out.println("Enter the quantity of numbers ");
+            System.out.println("Enter the numbers in massive: ");
+            for (int i = 0; i < mas.length; i++) {
+                mas[i] = scanner.nextInt();
+            }
 
-        int a = s.nextInt();
-
-        int[] mas = new int[a];
-        int max = mas[0];
-
-        System.out.println("Enter the numbers in massive: ");
-        for (int i = 0; i < mas.length; i++) {
-            mas[i] = s.nextInt();
-        }
-
-        for (int i = 0; i < mas.length; i++) {
-            max = Math.max(mas[i], max);
+            for (int i = 0; i < mas.length; i++) {
+                max = Math.max(mas[i], max);
 
 //           if (mas[i] > max)
 //               max = mas[i];
-        }
+            }
 
             System.out.println("Max number " + max);
 //        System.out.println("Min number " + min);
         }
+        else {
+            System.out.println("invalid input! ");
+        }
     }
+}
 
